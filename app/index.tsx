74e0a6cf6f66ba94aeac35de8049914a095242c0
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2025-04-01 14:43:07 +1200

    prototype landing page

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,10 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      className="bg-usp-dark-teal-500 h-full"
+      className="flex bg-usp-dark-teal-500 items-center justify-center h-full"
     >
-      <Text className="text-teal-400">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-usp-teal-300 font-bold text-6xl text-center">Welcome to</Text>
+      <Text className="text-usp-teal-500 font-bold text-8xl text-center">USPEnrol</Text>
     </View>
   );
 }
