efe8b6b3efd6f5eda3e5096920894b4eaf746afb
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2025-04-01 14:25:29 +1200

    setup react-native boiler plate code

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="bg-slate-900 h-full"
+    >
+      <Text className="text-teal-400">Login Screen here [TODO BY PRANAV]</Text>
+    </View>
+  );
+}
