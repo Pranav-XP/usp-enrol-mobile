d378dfa40d355a783855b847b7e1fd6b4d04b7bb
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2025-04-11 16:34:41 +1200

    V2.0 Added refresh button

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,7 +1,7 @@
 import { getProfile } from "@/api/api";
 import StudentProfile from "@/components/StudentProfile";
 import { useSession } from "@/context/ctx";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query"; // import useQueryClient
 import { View } from "react-native";
 import { Text, Button, ActivityIndicator, useTheme } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -10,23 +10,16 @@ export default function Profile() {
   const insets = useSafeAreaInsets();
   const { user, signOut, session } = useSession();
   const theme = useTheme();
+  const queryClient = useQueryClient(); // get queryClient instance
 
   const { data, isLoading, isError } = useQuery({
     queryKey: ["profile"],
     queryFn: () => getProfile(session),
   });
 
-  console.log(data);
   if (isLoading) {
-    // Show loading indicator
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator size="large" />
         <Text>Loading the profile...</Text>
       </View>
@@ -52,17 +45,13 @@ export default function Profile() {
         >
           Error loading data. Please try again.
         </Text>
-        <Button
-          onPress={() => {
-            signOut();
-          }}
-          mode="contained"
-        >
+        <Button onPress={signOut} mode="contained">
           Logout
         </Button>
       </View>
     );
   }
+
   return (
     <View
       style={{
@@ -75,12 +64,18 @@ export default function Profile() {
       }}
     >
       <StudentProfile student={data.student} />
+
       <Button
+        style={{ marginTop: 10 }}
         onPress={() => {
-          signOut();
+          queryClient.invalidateQueries(); // this line invalidates all queries
         }}
-        mode="contained"
+        mode="outlined"
       >
+        Refresh All Data
+      </Button>
+
+      <Button style={{ marginTop: 10 }} onPress={signOut} mode="contained">
         Logout
       </Button>
     </View>
