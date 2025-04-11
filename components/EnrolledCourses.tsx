5ddaa26ba69bbe449743a7e439d4f2d7c2c4b1b0
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2025-04-11 13:33:24 +1200

    V1.0 Added message for no available courses

--- components/EnrolledCourses.tsx
+++ components/EnrolledCourses.tsx
@@ -1,31 +1,38 @@
-import { View, Text } from "react-native";
-import { Divider, List } from "react-native-paper";
+import { View } from "react-native";
+import { Divider, List, Text, useTheme } from "react-native-paper";
 import React from "react";
 import { StudentCourse } from "@/api/interfaces";
 
 const EnrolledCourses = ({ courses }: { courses: StudentCourse[] }) => {
+  const enrolledCourses = courses.filter(
+    (course) => course.pivot.status === "enrolled"
+  );
+
+  const theme = useTheme();
+
   return (
     <View>
       <List.Accordion
-        title={`Enrolled Courses`}
-        left={(props) => (
-          <List.Icon
-            {...props}
-            icon={"timer-sand"} // Conditional icon based on grade
-          />
-        )}
+        title="Enrolled Courses"
+        left={(props) => <List.Icon {...props} icon="timer-sand" />}
       >
-        {courses
-          .filter((course) => course.pivot.status === "enrolled") // Filter enrolled courses
-          .map((course) => (
+        {enrolledCourses.length === 0 ? (
+          <View style={{ padding: 16 }}>
+            <Text style={{ color: theme.colors.onSurfaceVariant }}>
+              No enrolled courses
+            </Text>
+          </View>
+        ) : (
+          enrolledCourses.map((course) => (
             <View key={course.id}>
               <List.Item
                 title={course.course_code}
                 description={course.course_title}
               />
-              <Divider></Divider>
+              <Divider />
             </View>
-          ))}
+          ))
+        )}
       </List.Accordion>
     </View>
   );
